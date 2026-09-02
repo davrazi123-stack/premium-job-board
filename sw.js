9356fdf73e4289ee0b9ab541086daa22bd0461c4
David <davrazi123@gmail.com>
2026-09-02 14:40:33 +0200

    Fixed blocking service worker

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
+// Clean Passthrough Service Worker
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim());
 });
 
-// PWABuilder requires a fetch event listener to pass the test
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response('You are currently offline.');
-    })
-  );
+    // Just pass through normally without blocking
+    return;
 });
